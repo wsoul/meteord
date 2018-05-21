8cda957778aba306e1cc57b04f4083da50869b0b
Ivan Khyzhniak <wsoulbox@gmail.com>
2018-05-21 16:44:49 +0300

    fix

--- base/scripts/lib/install_additional.sh
+++ base/scripts/lib/install_additional.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-apt-get install python-software-properties software-properties-common
-add-apt-repository ppa:rwky/graphicsmagick
+add-apt-repository ppa:rwky/graphicsmagick -y
 apt-get update -y
 apt-get install -y graphicsmagick
 apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
